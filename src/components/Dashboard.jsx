801e829470cfb87c2defa43ded9083e0a57dd2ec
annu-adi <rashmi.maurya10@gmail.com>
2026-07-19 16:57:42 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -19,8 +19,7 @@ const Dashboard = () => {
   useEffect(()=>{
     const fetchAppointments = async () =>{
       try{
-        const {data} = await axios.get(
-        "http://localhost:4000/api/v1/appointment/getall", {
+        const {data} = await axios.get("https://vercel-backed1.vercel.app/api/v1/appointment/getall", {
           withCredentials:true}
         
         );
