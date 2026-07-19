a4d2f18d67c539394df6fa9a980fcc390c12cc85
annu-adi <rashmi.maurya10@gmail.com>
2026-07-19 16:58:37 +0530

    Update API endpoint for fetching doctors

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -13,7 +13,7 @@ const Doctors = () => {
    const fetchDoctors = async () =>{
 try{
   const {data} = await axios.get(
-     "http://localhost:4000/api/v1/user/doctors",
+     "https://vercel-backed1.vercel.app/api/v1/user/doctors",
      {withCredentials:true}
   );
    setDoctors(data.doctors);
